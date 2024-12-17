c2563cc06c970eb17e7eed2e4e8c1f0b0d5b3970
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-12-18 02:41:40 +0900

    test: 계산기 인풋필드에 use client 추가

--- src/app/(features)/calculator/DynamicChildren.tsx
+++
@@ -1,9 +0,0 @@
-// calculator/DynamicChildren.tsx
-import React, { ReactNode } from "react";
-
-// children의 타입을 ReactNode로 정의
-const DynamicChildren = ({ children }: { children: ReactNode }) => {
-  return <>{children}</>;
-};
-
-export default DynamicChildren;
